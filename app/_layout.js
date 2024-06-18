5986bc0c9556fc43f66612dc4129cc52e5de8ffd
Amogh Krishna <amoghkrishna55@gmail.com>
2024-06-18 18:30:02 +0100

    refactor: Update SOS component to use expo-haptics for haptic feedback

--- app/_layout.js
+++ app/_layout.js
@@ -1,25 +1,22 @@
 import { Stack } from "expo-router";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const RootLayer = () => {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerTitle: "SixthSense" }} />
-        <Stack.Screen
-          name="chat/index"
-          options={{ headerTitle: "Sixth Sense" }}
-        />
-        <Stack.Screen
-          name="info/index"
-          options={{ headerTitle: "Surrounding Sensor" }}
-        />
-        <Stack.Screen
-          name="location/index"
-          options={{ headerTitle: "Location" }}
-        />
-      </Stack>
-    </GestureHandlerRootView>
+    <Stack>
+      <Stack.Screen name="index" options={{ headerTitle: "SixthSense" }} />
+      <Stack.Screen
+        name="chat/index"
+        options={{ headerTitle: "Sixth Sense" }}
+      />
+      <Stack.Screen
+        name="vision/index"
+        options={{ headerTitle: "Surrounding Sensor" }}
+      />
+      <Stack.Screen
+        name="location/index"
+        options={{ headerTitle: "Location" }}
+      />
+    </Stack>
   );
 };
 
