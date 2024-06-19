5804f9c4e03871c21c466506629a46a323f3cc49
Amogh Krishna <amoghkrishna55@gmail.com>
2024-06-19 19:36:25 +0100

    refactor: Remove commented code in _layout.js

--- app/_layout.js
+++ app/_layout.js
@@ -1,23 +1,20 @@
-import { Stack } from "expo-router";
+// import { Stack } from "expo-router";
 
-const RootLayer = () => {
-  return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerTitle: "SixthSense" }} />
-      <Stack.Screen
-        name="chat/index"
-        options={{ headerTitle: "Sixth Sense" }}
-      />
-      <Stack.Screen
-        name="vision/index"
-        options={{ headerTitle: "Surrounding Sensor" }}
-      />
-      <Stack.Screen
-        name="location/index"
-        options={{ headerTitle: "Location" }}
-      />
-    </Stack>
-  );
-};
+// const RootLayer = () => {
+//   return (
+//     <Stack>
+//       <Stack.Screen name="index" options={{ headerTitle: "SixthSense" }} />
+//       <Stack.Screen
+//         name="chat/index"
+//         options={{ headerTitle: "Sixth Sense" }}
+//       />
+//       <Stack.Screen name="vision/index" options={{ headerTitle: "Vision" }} />
+//       <Stack.Screen
+//         name="location/index"
+//         options={{ headerTitle: "Location" }}
+//       />
+//     </Stack>
+//   );
+// };
 
-export default RootLayer;
+// export default RootLayer;
