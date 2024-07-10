ff713db8490afa88442113ed101a0f131962859c
Amogh Krishna <amoghkrishna55@gmail.com>
2024-07-10 22:09:21 +0100

    chore: Add lazy loading for images in Message component

--- app/message/index.js
+++ app/message/index.js
@@ -1,13 +1,33 @@
-import {ref, push} from 'firebase/database';
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  ScrollView,
+  StyleSheet,
+} from 'react-native';
+import {ref, push, onValue} from 'firebase/database';
 import {database} from '../../components/firebase';
-import React, {useState} from 'react';
-import {View, Text, TextInput, Button, StyleSheet} from 'react-native';
 
 const Message = ({navigation}) => {
   const [inputText, setInputText] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const dbRef = ref(database, 'messages');
+    const listener = onValue(dbRef, snapshot => {
+      const messagesArray = [];
+      const data = snapshot.val();
+      for (let id in data) {
+        messagesArray.push({id, ...data[id]});
+      }
+      setMessages(messagesArray);
+    });
+    return () => listener();
+  }, []);
 
   const handlePress = () => {
-    // Handle the button press here
     if (inputText !== '') {
       const dbRef = ref(database, 'messages');
       push(dbRef, {
@@ -17,10 +37,15 @@ const Message = ({navigation}) => {
       }).catch(error => {
         console.error('Error adding data: ', error);
       });
-      console.log('Data added');
       setInputText('');
-      navigation.navigate('Intersection');
     }
+  };
+
+  const formatTime = timestamp => {
+    const date = new Date(timestamp);
+    return `${date.getDate()}/${
+      date.getMonth() + 1
+    }/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
   };
 
   return (
@@ -33,6 +58,17 @@ const Message = ({navigation}) => {
         placeholder="Enter your message"
       />
       <Button title="Submit" onPress={handlePress} />
+      <ScrollView style={styles.messagesContainer}>
+        {[...messages].reverse().map(message => (
+          <View key={message.id} style={styles.messageContainer}>
+            <Text style={styles.messageText}>{message.text}</Text>
+            <Text style={styles.messageInfo}>
+              Time: {formatTime(message.time)}
+            </Text>
+            <Text style={styles.messageInfo}>Status: {message.status}</Text>
+          </View>
+        ))}
+      </ScrollView>
       <Button
         title="Back"
         onPress={() => navigation.navigate('Intersection')}
@@ -55,6 +91,23 @@ const styles = StyleSheet.create({
     margin: 12,
     borderWidth: 1,
     padding: 10,
+    width: '80%',
+  },
+  messagesContainer: {
+    width: '90%',
+    maxHeight: '50%',
+  },
+  messageContainer: {
+    padding: 10,
+    marginVertical: 5,
+    backgroundColor: '#f0f0f0',
+    borderRadius: 5,
+  },
+  messageText: {
+    fontSize: 16,
+  },
+  messageInfo: {
+    fontSize: 12,
   },
 });
 
