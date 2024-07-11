ce6c63b3684830b316e3b5cd0bbb636ffbefa9a4
Amogh Krishna <amoghkrishna55@gmail.com>
2024-07-11 10:15:28 +0100

    chore: Update Boundary component to include map view and location update on drag

--- app/boundary/index.js
+++ app/boundary/index.js
@@ -1,8 +1,10 @@
 import React, {useState} from 'react';
-import {View, TextInput, Button, StyleSheet} from 'react-native';
+import {View, TextInput, StyleSheet} from 'react-native';
 import MapView, {Circle, Marker} from 'react-native-maps';
 import {database} from '../../components/firebase';
 import {ref, update} from 'firebase/database';
+import Button from '../../components/button';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const Boundary = ({navigation, route}) => {
   const {latitude, longitude, radius} = route.params;
@@ -39,8 +41,9 @@ const Boundary = ({navigation, route}) => {
             const newCoords = e.nativeEvent.coordinate;
             setLatitude(newCoords.latitude);
             setLongitude(newCoords.longitude);
-          }}
-        />
+          }}>
+          <Ionicons name="location" size={50} color="black" />
+        </Marker>
         <Circle
           center={{
             latitude: newlatitude,
@@ -52,21 +55,33 @@ const Boundary = ({navigation, route}) => {
           fillColor="rgba(255, 0, 0, 0.5)"
         />
       </MapView>
-
-      <TextInput
-        style={styles.input}
-        placeholder={`Radius (in meters) : ${radius} `}
-        value={radius}
-        onChangeText={e => {
-          if (e != '') {
-            setRadius(parseInt(e));
-          } else {
-            setRadius(radius);
-          }
-        }}
-        keyboardType="numeric"
-      />
-      <Button title="Submit" onPress={handleSubmit} />
+      <View style={styles.buttonContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder={`Radius (in meters) : ${radius} `}
+          value={radius}
+          onChangeText={e => {
+            if (e != '') {
+              setRadius(parseInt(e));
+            } else {
+              setRadius(radius);
+            }
+          }}
+          keyboardType="numeric"
+        />
+        <Button
+          text="Set Boundary "
+          onPress={handleSubmit}
+          Ion="refresh-circle-outline"
+        />
+      </View>
+      <View style={styles.buttonContainerTop}>
+        <Button
+          text="Back "
+          onPress={() => navigation.navigate('Location')}
+          Ion={'arrow-back-outline'}
+        />
+      </View>
     </View>
   );
 };
@@ -74,20 +89,40 @@ const Boundary = ({navigation, route}) => {
 const styles = StyleSheet.create({
   input: {
     height: 40,
-    marginVertical: 12,
-    borderWidth: 1,
+    borderWidth: 2,
+    marginHorizontal: '10%',
+    borderColor: '#292927', // A nice shade of blue for the border
+    borderRadius: 10, // Rounded corners
     padding: 10,
+    width: '80%', // Adjust as needed
+    fontSize: 16, // Larger font size for readability
+    backgroundColor: '#F0F0F0', // Soft background color
+    color: '#333333', // Text color
+    // Placeholder text color is platform-specific and might need to be handled differently
   },
   container: {
     flex: 1,
   },
   map: {
     flex: 1,
-    // position: "absolute",
     left: 0,
     right: 0,
     top: 0,
     bottom: 0,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 10,
+    left: 0,
+    right: 0, // Added to make the container full width
+    zIndex: 100, // Ensure the button is on top of the map
+  },
+  buttonContainerTop: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    // right: 0, // Added to make the container full width
+    // zIndex: 100, // Ensure the button is on top of the map
   },
 });
 
