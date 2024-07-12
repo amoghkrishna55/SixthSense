be69c71ef225af99514a03a08e7191919c6f8146
Amogh Krishna <amoghkrishna55@gmail.com>
2024-07-12 21:36:17 +0100

    chore: Update background color in sos.js component

--- components/sos.js
+++ components/sos.js
@@ -74,7 +74,7 @@ const SOS = forwardRef(({children}, ref) => {
           ref={ref}
           style={{
             position: 'absolute',
-            backgroundColor: 'red',
+            backgroundColor: '#292927',
             borderRadius: 1000,
             width: sizeAnim,
             height: sizeAnim,
